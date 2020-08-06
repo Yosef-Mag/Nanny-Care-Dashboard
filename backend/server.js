09ad6bb7fdb37b3a2e7b30d4ff9738a3f4d3ca0a
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-08-06 07:36:32 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const cors = require("cors");
 // const router = express.Router();
 var Adminhandlers = require("./handlers/Adminhandlers");
 
-
 var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,16 +27,15 @@ app.use(
 );
 var port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 require("dotenv").config(); // to read .env file
-
 
 app.post("/AddAdmin", Adminhandlers.adminSignUp);
 app.post("/adminLogin", Adminhandlers.adminLogIn);
