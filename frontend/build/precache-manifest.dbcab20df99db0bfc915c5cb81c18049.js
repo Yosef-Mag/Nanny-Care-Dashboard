c835bf19c47892ed7e6e837d8b70d801e5eb9e9f
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-08-08 12:40:17 +0300

    back to localhost

---
+++ frontend/build/precache-manifest.dbcab20df99db0bfc915c5cb81c18049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5641e3f9df91b9aad322bbd7cc944663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a0fd5162aac29973a1",
+    "url": "/static/js/2.aa985912.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.aa985912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4f9b489ca30bb14ffc",
+    "url": "/static/js/main.73b7e72e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5ed8f3ff0dd4fe476756c98a7a8ae9e3",
+    "url": "/static/media/nanny-care.5ed8f3ff.png"
+  }
+]);
---
+++ frontend/src/components/Admin.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function AllNanny(props) {
+  const [nannylist, setNannylist] = useState([]); // state to hold all nanny records data
+  useEffect(() => {
+    fetch(`https://localhost:5000/Admin`)
+      .then((res) => res.json())
+      .then((response) => {
+        setNannylist(response);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const addNanny = () => {
+    props.history.push("/AddNanny");
+  };
+
+  const deleteNanny = (_id) => {
+    console.log(_id);
+    return axios
+      .delete("https://localhost:5000/delete/" + _id)
+      .then((props) => window.location.reload())
+      .catch((err) => console.log(err.response));
+  };
+
+  console.log(nannylist);
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <>
+      <br /> <br /> <br /> <br />
+      <Button
+        onClick={addNanny}
+        style={{
+          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+          borderRadius: 3,
+          border: 0,
+          color: "white",
+          height: 48,
+          padding: "0 30px",
+          boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+          marginLeft: "750px",
+        }}
+      >
+        {" "}
+        Add new nanny{" "}
+      </Button>
+      <Grid>
+        <br /> <br />
+        {nannylist.map((nany) => (
+          <Card className={classes.root} variant="outlined">
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt={nany.name} src={nany.image} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={nany.name}
+                secondary={
+                  <React.Fragment>
+                    <Typography component="span">
+                      Place : {nany.place}
+                      <br />
+                    </Typography>
+                    <Typography component="span">
+                      Email : {nany.email}{" "}
+                    </Typography>
+                    <br />
+                    <Typography component="span">
+                      phoneNumber : {nany.phoneNumber}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Button onClick={() => deleteNanny(nany._id)}> Delete </Button>
+          </Card>
+        ))}
+      </Grid>
+    </>
+  );
+}
